e0bf5bb2679bf53f1f18b1c44a150459f39eefa0
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-29 18:33:17 +0000

    update unitial users

--- users.js
+++ users.js
@@ -5,6 +5,7 @@ export const users = [
     bookmarks: [
       {
         id: 1,
+        date: "JAN 29, 2025",
         url: "https://codeyourfuture.io/",
         title: "The beginning of the end",
         description: "The end is very near. People should harry up",
@@ -18,6 +19,7 @@ export const users = [
     bookmarks: [
       {
         id: 1,
+        date: "JAN 29, 2025",
         url: "https://www.codewars.com/",
         title: "Lif if about saying HELLO",
         description: "HELLO is used every in the workplace",
