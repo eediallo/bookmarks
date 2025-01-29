abff6cc0204ee0f434f8c9ef8e7779696b160f39
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-29 16:35:42 +0000

    create initial users

---
+++ users.js
@@ -0,0 +1,27 @@
+export const users = [
+  {
+    id: 1,
+    name: "Elhadj",
+    bookmarks: [
+      {
+        id: 1,
+        url: "example.com",
+        title: "The beginning of the end",
+        description: "The end is very near. People should harry up",
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    name: "Sally",
+    bookmarks: [
+      {
+        id: 1,
+        url: "hello.com",
+        title: "Lif if about saying HELLO",
+        description: "HELLO is used every in the workplace",
+      },
+    ],
+  },
+];
