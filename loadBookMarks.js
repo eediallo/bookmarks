4c2c65dbe7984daa4551fc8adea8c55e50b39b4a
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-30 16:29:09 +0000

    refactor -- > create loadBookmarksForSelectedUser function to laod bookmarks

---
+++ loadBookMarks.js
@@ -0,0 +1,29 @@
+import { getUserIds, getData } from "./storage.js";
+import { createBookmarkCard } from "./script.js";
+const userSelectEl = document.getElementById("user");
+
+export function loadBookmarksForSelectedUser() {
+  const optionText = userSelectEl.value.toLowerCase();
+  const bookmarksContainer = document.querySelector(".bookmarks");
+  bookmarksContainer.innerHTML = ""; // Clear previous bookmarks
+
+  const userIds = getUserIds();
+  let userFound = false;
+  for (const id of userIds) {
+    const userData = getData(id);
+    if (
+      userData &&
+      userData.name &&
+      userData.name.toLowerCase() === optionText
+    ) {
+      userFound = true;
+      if (userData.bookmarks && userData.bookmarks.length > 0) {
+        const bookmarkCard = createBookmarkCard(userData);
+        bookmarksContainer.append(bookmarkCard);
+      }
+    }
+  }
+  if (!userFound || bookmarksContainer.children.length === 0) {
+    bookmarksContainer.innerHTML = `<p>No bookmarks available for <strong>${optionText}</strong></p>`;
+  }
+}
