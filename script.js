987a79fbc2c4acffdb7f1c255b6d2b622bf0b952
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-30 17:26:07 +0000

    refactor by creating functions main and showBookmarkForm

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ import { loadBookmarksForSelectedUser } from "./loadBookMarks.js";
 import { userSelectEl, formEl, createBookmarkBtn } from "./config.js";
 import { renderOptionList } from "./option.js";
 
-window.onload = function () {
-  createBookmarkBtn.addEventListener("click", () => {
-    formEl.style.display = "block";
-    // event for bookmark submission
-    formEl.addEventListener("submit", handleBookmarkSubmission);
-    createBookmarkBtn.style.display = "none";
-  });
+function showBookmarkForm() {
+  formEl.style.display = "block";
+  createBookmarkBtn.style.display = "none";
+}
 
+function main() {
   renderOptionList();
+  createBookmarkBtn.addEventListener("click", showBookmarkForm);
+  formEl.addEventListener("submit", handleBookmarkSubmission);
   userSelectEl.addEventListener("change", loadBookmarksForSelectedUser);
-};
+}
+
+window.onload = main;
