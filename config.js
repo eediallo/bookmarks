1944bc188fe4afbae1c18099b9700924b382fb31
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-30 16:57:38 +0000

    move dom queries to config fille

---
+++ config.js
@@ -0,0 +1,7 @@
+const formEl = document.querySelector(".form");
+const userSelectEl = document.getElementById("user");
+const createBookmarkBtn = document.querySelector(".create-bookmark-btn");
+
+formEl.style.display = "none";
+
+export { formEl, userSelectEl, createBookmarkBtn };
