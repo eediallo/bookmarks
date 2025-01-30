9389d1a2574d484ff8605b43000753f677ccf298
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-30 17:39:37 +0000

    update event handling to use toggleBookmarkFormAndButtonVisibility function

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 import { handleBookmarkSubmission } from "./handleBookMarksSub.js";
 import { loadBookmarksForSelectedUser } from "./loadBookMarks.js";
-import { userSelectEl, formEl, createBookmarkBtn } from "./config.js";
+import {
+  userSelectEl,
+  formEl,
+  createBookmarkBtn,
+  toggleBookmarkFormAndButtonVisibility,
+} from "./config.js";
 import { renderOptionList } from "./option.js";
-
-function showBookmarkForm() {
-  formEl.style.display = "block";
-  createBookmarkBtn.style.display = "none";
-}
 
 function main() {
   renderOptionList();
-  createBookmarkBtn.addEventListener("click", showBookmarkForm);
+  createBookmarkBtn.addEventListener("click", () => {
+    toggleBookmarkFormAndButtonVisibility(false);
+  });
   formEl.addEventListener("submit", handleBookmarkSubmission);
   userSelectEl.addEventListener("change", loadBookmarksForSelectedUser);
 }
