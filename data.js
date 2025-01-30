023de1dd3e1fceee1c5e6b71612071982189d958
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-30 13:36:35 +0000

    removes initial bookmarks and new three more users

--- data.js
+++ data.js
@@ -1,27 +1,59 @@
-export const INITIAL_USERS = [
+// export const INITIAL_USERS = [
+//   {
+//     id: 1,
+//     name: "Elhadj",
+//     bookmarks: [
+//       {
+//         date: "JAN 29, 2025",
+//         url: "https://codeyourfuture.io/",
+//         title: "The beginning of the end",
+//         description: "The end is very near. People should harry up",
+//       },
+//     ],
+//   },
+
+//   {
+//     id: 2,
+//     name: "Sally",
+//     bookmarks: [
+//       {
+//         date: "JAN 29, 2025",
+//         url: "https://www.codewars.com/",
+//         title: "Lif if about saying HELLO",
+//         description: "HELLO is used every in the workplace",
+//       },
+//     ],
+//   },
+// ];
+
+export const USERS = [
   {
     id: 1,
     name: "Elhadj",
-    bookmarks: [
-      {
-        date: "JAN 29, 2025",
-        url: "https://codeyourfuture.io/",
-        title: "The beginning of the end",
-        description: "The end is very near. People should harry up",
-      },
-    ],
+    bookmarks: [],
   },
 
   {
     id: 2,
     name: "Sally",
-    bookmarks: [
-      {
-        date: "JAN 29, 2025",
-        url: "https://www.codewars.com/",
-        title: "Lif if about saying HELLO",
-        description: "HELLO is used every in the workplace",
-      },
-    ],
+    bookmarks: [],
+  },
+
+  {
+    id: 3,
+    name: "Daniel",
+    bookmarks: [],
+  },
+
+  {
+    id: 4,
+    name: "Fatma",
+    bookmarks: [],
+  },
+
+  {
+    id: 5,
+    name: "Donara",
+    bookmarks: [],
   },
 ];
