c0c8300252a09345a3365b3665ccb9c63126b365
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-02 11:31:50 +0000

    refactor: remove commented-out code in eventHandlers.js

--- eventHandlers.js
+++ eventHandlers.js
@@ -1,15 +1,8 @@
 import {formEl, userSelectEl } from "./config.js";
 import { handleBookmarkSubmission } from "./handleBookMarksSub.js";
 import { loadBookmarksForSelectedUser } from "./loadBookMarks.js";
-// import { toggleBookmarkFormAndButtonVisibility } from "./config.js";
 
 export function setupEventListeners() {
-  //   if (createBookmarkBtn) {
-  //     createBookmarkBtn.addEventListener("click", () => {
-  //       toggleBookmarkFormAndButtonVisibility(false);
-  //     });
-  //   }
-
   if (formEl) {
     formEl.addEventListener("submit", handleBookmarkSubmission);
   }
