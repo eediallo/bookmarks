f1142ebcb3649bda4717ef18762c4a7efe4d77c0
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-02 10:49:23 +0000

    add event handler setup for bookmark functionality

---
+++ eventHandlers.js
@@ -0,0 +1,20 @@
+import { createBookmarkBtn, formEl, userSelectEl } from "./config.js";
+import { handleBookmarkSubmission } from "./handleBookMarksSub.js";
+import { loadBookmarksForSelectedUser } from "./loadBookMarks.js";
+import { toggleBookmarkFormAndButtonVisibility } from "./config.js";
+
+export function setupEventListeners() {
+  if (createBookmarkBtn) {
+    createBookmarkBtn.addEventListener("click", () => {
+      toggleBookmarkFormAndButtonVisibility(false);
+    });
+  }
+
+  if (formEl) {
+    formEl.addEventListener("submit", handleBookmarkSubmission);
+  }
+
+  if (userSelectEl) {
+    userSelectEl.addEventListener("change", loadBookmarksForSelectedUser);
+  }
+}
