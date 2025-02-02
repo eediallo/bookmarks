8a251ca5e7e1405f64403642a9222bac2b328f4c
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-02 11:26:06 +0000

    comment out unused bookmark button event listener setup

--- eventHandlers.js
+++ eventHandlers.js
@@ -1,14 +1,14 @@
-import { createBookmarkBtn, formEl, userSelectEl } from "./config.js";
+import {formEl, userSelectEl } from "./config.js";
 import { handleBookmarkSubmission } from "./handleBookMarksSub.js";
 import { loadBookmarksForSelectedUser } from "./loadBookMarks.js";
-import { toggleBookmarkFormAndButtonVisibility } from "./config.js";
+// import { toggleBookmarkFormAndButtonVisibility } from "./config.js";
 
 export function setupEventListeners() {
-  if (createBookmarkBtn) {
-    createBookmarkBtn.addEventListener("click", () => {
-      toggleBookmarkFormAndButtonVisibility(false);
-    });
-  }
+  //   if (createBookmarkBtn) {
+  //     createBookmarkBtn.addEventListener("click", () => {
+  //       toggleBookmarkFormAndButtonVisibility(false);
+  //     });
+  //   }
 
   if (formEl) {
     formEl.addEventListener("submit", handleBookmarkSubmission);
