db4a09732ec4bdc7bc122466cbf2db1cee620f30
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-02 11:25:45 +0000

    comment-out code for bookmark form visibility management

--- config.js
+++ config.js
@@ -2,21 +2,20 @@ const formEl = document.querySelector(".form");
 const userSelectEl = document.getElementById("user");
 const createBookmarkBtn = document.querySelector(".create-bookmark-btn");
 
-formEl.style.display = "none";
+//formEl.style.display = "none";
 
-function toggleBookmarkFormAndButtonVisibility(isDisplayed) {
-  if (!isDisplayed) {
-    formEl.style.display = "block";
-    createBookmarkBtn.style.display = "none";
-  } else {
-    formEl.style.display = "none";
-    createBookmarkBtn.style.display = "block";
-  }
-}
+// function toggleBookmarkFormAndButtonVisibility(isDisplayed) {
+//   if (!isDisplayed) {
+//     formEl.style.display = "block";
+//     createBookmarkBtn.style.display = "none";
+//   } else {
+//     formEl.style.display = "none";
+//     createBookmarkBtn.style.display = "block";
+//   }
+// }
 
 export {
   formEl,
   userSelectEl,
   createBookmarkBtn,
-  toggleBookmarkFormAndButtonVisibility,
 };
