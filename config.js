0f38947a8909a49fd6591cd503900b85882440b0
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-02 11:31:40 +0000

    refactor: clean up config.js by removing commented-out code

--- config.js
+++ config.js
@@ -2,20 +2,4 @@ const formEl = document.querySelector(".form");
 const userSelectEl = document.getElementById("user");
 const createBookmarkBtn = document.querySelector(".create-bookmark-btn");
 
-//formEl.style.display = "none";
-
-// function toggleBookmarkFormAndButtonVisibility(isDisplayed) {
-//   if (!isDisplayed) {
-//     formEl.style.display = "block";
-//     createBookmarkBtn.style.display = "none";
-//   } else {
-//     formEl.style.display = "none";
-//     createBookmarkBtn.style.display = "block";
-//   }
-// }
-
-export {
-  formEl,
-  userSelectEl,
-  createBookmarkBtn,
-};
+export { formEl, userSelectEl, createBookmarkBtn };
