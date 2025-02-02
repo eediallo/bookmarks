4b07f459758b92119c3018078a0c1c6487b607bc
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-02 11:54:08 +0000

    test: add validation for USERS to ensure each entry is a plain object

--- users.test.js
+++ users.test.js
@@ -1,5 +1,15 @@
 import { USERS } from "./data.js";
 
+function isPlainObject(value) {
+  return (
+    typeof value === "object" &&
+    value !== null &&
+    Object.prototype.toString.call(value) === "[object Object]" &&
+    (Object.getPrototypeOf(value) === Object.prototype ||
+      Object.getPrototypeOf(value) === null)
+  );
+}
+
 describe("USERS", () => {
   test("should not be empty", () => {
     const currentInput = USERS.length > 0;
@@ -9,6 +19,15 @@ describe("USERS", () => {
 
   test("should have exactly 5 users", () => {
     const currentInput = USERS.length === 5;
+    const targetInput = true;
+    expect(currentInput).toBe(targetInput);
+  });
+
+  test("Each each should be of type plain object", () => {
+    let currentInput = false;
+    for (const user of USERS) {
+      currentInput = isPlainObject(user)
+    }
     const targetInput = true;
     expect(currentInput).toBe(targetInput);
   });
