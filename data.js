60a87cf602b7f3983a9198692d95db2d15e1cbf0
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-15 12:10:46 +0000

    refactor: remove commented-out initial user data from data.js

--- data.js
+++ data.js
@@ -1,31 +1,3 @@
-// export const INITIAL_USERS = [
-//   {
-//     id: 1,
-//     name: "Elhadj",
-//     bookmarks: [
-//       {
-//         date: "JAN 29, 2025",
-//         url: "https://codeyourfuture.io/",
-//         title: "The beginning of the end",
-//         description: "The end is very near. People should harry up",
-//       },
-//     ],
-//   },
-
-//   {
-//     id: 2,
-//     name: "Sally",
-//     bookmarks: [
-//       {
-//         date: "JAN 29, 2025",
-//         url: "https://www.codewars.com/",
-//         title: "Lif if about saying HELLO",
-//         description: "HELLO is used every in the workplace",
-//       },
-//     ],
-//   },
-// ];
-
 export const USERS = [
   {
     id: 1,
