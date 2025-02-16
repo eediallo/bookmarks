e9f5cf0fc4a457696d569a12d39231a93b0db75a
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-16 17:53:20 +0000

    refactor: add bookmark title creation for selected user in loadBookmarksForSelectedUser

--- eventHandlers/loadBookMarks.js
+++ eventHandlers/loadBookMarks.js
@@ -1,6 +1,7 @@
 import { getUserIds, getData } from "../data/storage.js";
 import { renderBookmarks } from "../UI/bookMarkCard.js";
 import { bookmarksContainer, userSelectEl } from "../domQueries.js";
+import { createElement } from "../UI/createElement.js";
 
 export function loadBookmarksForSelectedUser() {
   const optionText = userSelectEl.value.toLowerCase();
@@ -16,6 +17,10 @@ export function loadBookmarksForSelectedUser() {
       userData.name.toLowerCase() === optionText
     ) {
       userFound = true;
+
+      const bookmarkTitle = createElement(`${userData.name}'s bookmarks`, "h1");
+      bookmarksContainer.append(bookmarkTitle);
+
       if (userData.bookmarks && userData.bookmarks.length > 0) {
         renderBookmarks(userData.bookmarks);
       }
