8bd67af37f8feb72a3e9f49ec3c4fd686f997492
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-16 20:11:44 +0000

    refactor: add event handler for addBookmarkBtn to toggle form display

--- eventHandlers/attachEventHandlers.js
+++ eventHandlers/attachEventHandlers.js
@@ -1,8 +1,12 @@
-import { formEl, userSelectEl } from "../domQueries.js";
+import { addBookmarkBtn, formEl, userSelectEl } from "../domQueries.js";
 import { handleBookmarkSubmission } from "./handleBookMarksSub.js";
 import { loadBookmarksForSelectedUser } from "./loadBookMarks.js";
+import { toggleFormDisplay } from "./toggleFormDisplay.js";
 
 export function attachEventHandlers() {
   formEl.addEventListener("submit", handleBookmarkSubmission);
   userSelectEl.addEventListener("change", loadBookmarksForSelectedUser);
+  addBookmarkBtn.addEventListener("click", () => {
+    toggleFormDisplay(false);
+  });
 }
